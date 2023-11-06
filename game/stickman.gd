b4cc0ca240a4259f9fb45c7e33361f4a6cae1cb0
krishthedish17 <krishr1702@gmail.com>
2023-11-05 21:21:43 -0500

    fixed movement note: plz fix respawn and death

---
+++ game/stickman.gd
@@ -0,0 +1,107 @@
+extends CharacterBody2D
+
+@export_category("Player Properties") # You can tweak these changes according to your likings
+@export var move_speed : float = 400
+@export var jump_force : float = 600
+@export var max_jump_count : int = 2
+var jump_count : int = 2
+
+@export_category("Toggle Functions") # Double jump feature is disable by default (Can be toggled from inspector)
+@export var double_jump : = false
+
+var is_grounded : bool = false
+
+@onready var player_sprite = $AnimatedSprite2D
+@onready var spawn_point = %SpawnPoint
+@onready var particle_trails = $ParticleTrails
+@onready var death_particles = $DeathParticles
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+
+# --------- VARIABLES ---------- #
+
+
+
+# --------- BUILT-IN FUNCTIONS ---------- #
+
+func _process(_delta):
+	# Calling functions
+	player_animations()
+	flip_player()
+	
+# --------- CUSTOM FUNCTIONS ---------- #
+
+# <-- Player Movement Code -->
+
+# Handle Player Animations
+func player_animations():
+	particle_trails = false
+	
+	if is_on_floor():
+		if abs(velocity.x) > 0:
+			particle_trails = true
+			player_sprite.play("Sprint", 1.5)
+		else:
+			player_sprite.play("Idle")
+	else:
+		player_sprite.play("Jump")
+
+# Flip player sprite based on X velocity
+func flip_player():
+	if velocity.x < 0: 
+		player_sprite.flip_h = true
+	elif velocity.x > 0:
+		player_sprite.flip_h = false
+
+# Tween Animations
+func death_tween():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2.ZERO, 0.15)
+	await tween.finished
+	global_position = spawn_point.global_position
+	await get_tree().create_timer(0.3).timeout
+	respawn_tween()
+
+func respawn_tween():
+	var tween = create_tween()
+	tween.stop(); tween.play()
+	tween.tween_property(self, "scale", Vector2.ONE, 0.15) 
+
+func jump_tween():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(0.7, 1.4), 0.1)
+	tween.tween_property(self, "scale", Vector2.ONE, 0.1)
+
+# --------- SIGNALS ---------- #
+
+# Reset the player's position to the current level spawn point if collided with any trap
+func _on_collision_body_entered(_body):
+	if _body.is_in_group("Traps"):
+		death_particles.emitting = true
+		death_tween()
+
+
